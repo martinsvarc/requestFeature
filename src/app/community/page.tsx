6f591bd7cab97d8ab9923562f02ec64c3301dbf1
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-21 20:41:28 +0100

    Update page.tsx

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,5 +1,11 @@
 import Support from './support'
 
 export default function Page() {
-  return <Support />
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <main className="py-8">
+        <Support />
+      </main>
+    </div>
+  )
 }
