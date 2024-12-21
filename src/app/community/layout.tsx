153ed5edc9f0f46eaeceed5a6d0152230bffc7f7
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-21 19:54:42 +0100

    Create layout.tsx

---
+++ src/app/community/layout.tsx
@@ -0,0 +1,11 @@
+import Support from './support'
+
+export default function CommunityLayout() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <main className="py-8">
+        <Support />
+      </main>
+    </div>
+  )
+}
